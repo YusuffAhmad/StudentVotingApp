af5ce3944d3a0520abf3c6149bf1e3b64661ff5d
YusuffAhmad <97904018+YusuffAhmad@users.noreply.github.com>
2022-02-19 11:32:51 +0100

    Program.cs

---
+++ StudentsVotingSystem/Program.cs
@@ -0,0 +1,24 @@
+﻿using StudentsVotingSystem.Models;
+using StudentsVotingSystem.Repository;
+using System;
+
+namespace StudentsVotingSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationContext _context = new();
+            ElectionRepo ElectionRepo = new ElectionRepo(_context);
+            PositionRepo positionRepo = new PositionRepo(_context);
+            VoteRepo voteRepo = new VoteRepo(_context, ElectionRepo);
+            CandidateRepo candidateRepo = new CandidateRepo(_context, positionRepo, ElectionRepo);
+            VoterRepo voterRepo = new VoterRepo(_context, ElectionRepo, positionRepo, voteRepo, candidateRepo);
+            AdminRepo AdminRepo = new AdminRepo(_context, ElectionRepo, voteRepo, positionRepo);
+            LoginMenu LoginMenu = new LoginMenu(_context, voterRepo, AdminRepo);
+            RegistrationMenu RegistrationMenu = new RegistrationMenu(_context, voterRepo, AdminRepo);
+            Menu menu = new Menu(_context, AdminRepo, LoginMenu, RegistrationMenu);
+            menu.MainMenu();
+        }
+    }
+}
